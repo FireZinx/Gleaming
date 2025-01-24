1eb9f4a02d10ed6de73035923cf757bffb84a15c
FireZin <murilo-henrique.gp@hotmail.com>
2025-01-24 13:00:06 -0300

    Update files

---
+++ src/components/DataContext.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState, createContext, Children, ChildContextProvider, Dispatch, SetStateAction, MutableRefObject} from "react"
+import styles from "../styles/Home.module.css"
+import { DefaultLoadingManager } from "three"
+
+type SceneContextData = {
+    view: null | string;
+    previousView: null | string;
+    returning: MutableRefObject<boolean>;
+    setView: (value: string) => void;
+    setPreviousView: (value: string) => void;
+    setReturning: (state: boolean) => void;
+}
+
+export const ButtonContext = createContext({} as SceneContextData)
+
+export function ButtonContextProvider({ children }, props) {
+    const [ currentViewPos, setCurrentViewPos ] = useState(null);
+    const [ previousViewPos, setPreviousViewPos ] = useState(null);
+
+    const returning = useRef(false);
+
+    const setView = (value: string) => {
+        setCurrentViewPos(value);
+    }
+
+    const setPreviousView = (value: string) => {
+        setPreviousViewPos(value);
+    }
+
+    const setReturning = (state: boolean) => {
+        returning.current = state;
+    }
+
+    return(
+        <ButtonContext.Provider value={{
+            view: currentViewPos,
+            previousView: previousViewPos,
+            returning,
+            setView,
+            setPreviousView,
+            setReturning
+        }}>
+            { children }
+        </ButtonContext.Provider>
+    )
+}
