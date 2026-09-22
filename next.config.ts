09135235931b8a6dff3578cd981f5977db78d7a3
FireZinx <murilo-henrique.gp@hotmail.com>
2026-09-22 20:23:17 -0300

    Minor changes

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,14 @@ import type { NextConfig } from "next";
 console.log("NEXT CONFIG LOADED");
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  allowedDevOrigins: [
-    "http://192.168.15.3:3000",
-    "http://localhost:3000",
-  ],
+  output: "export",
+
+  basePath: "/Gleaming",
+  assetPrefix: "/Gleaming/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
